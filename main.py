cfc732156e155315d0d185c6902b283b785d688e
tregzmusic <tregzmusic@gmail.com>
2023-03-15 21:33:35 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from aiogram import Bot, Dispatcher, executor, types
+from config import API_TOKEN
 
+API_TOKEN = API_TOKEN
+
+bot = Bot(API_TOKEN)
+disp = Dispatcher(bot)
+
+@disp.message_handler()
+async def echo_message(message: types.Message):
+    if message.text.count(' ') >= 1:  # if user > 1 word
+        await message.answer(text=message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(disp)
